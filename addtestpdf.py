9c9d433d13301eb888aaf6500c51c0d08aceadff
SASAKI <b1022112@fun.ac.jp>
2025-03-01 01:11:30 +0900

    add jsonl

--- addtestpdf.py
+++
@@ -1,8 +0,0 @@
-from fpdf import FPDF
-
-pdf = FPDF()
-pdf.add_page()
-pdf.set_font("Arial", size=12)
-pdf.cell(200, 10, txt="This is a test PDF file.", ln=True, align='C')
-
-pdf.output("C:/Users/sasak/tundereAI/local-rag/test.pdf")
